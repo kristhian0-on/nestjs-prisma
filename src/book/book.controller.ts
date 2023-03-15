ec144df110f5c18e0a4dcce8857996229f887de7
Cristiano <kristian0tj2019@gmail.com>
2023-03-15 00:35:54 -0300

    feat: routers delete, update and finAll

--- src/book/book.controller.ts
+++ src/book/book.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { BookService } from './book.service';
 import { BookDTO } from './book.dto';
 
@@ -9,5 +17,20 @@ export class BookController {
   @Post()
   async create(@Body() data: BookDTO) {
     return this.bookService.create(data);
+  }
+
+  @Get()
+  async findAll() {
+    return this.bookService.findAll();
+  }
+
+  @Put(':id')
+  async update(@Param('id') id: string, @Body() data: BookDTO) {
+    return this.bookService.update(id, data);
+  }
+
+  @Delete(':id')
+  async delete(@Param('id') id: string) {
+    return this.bookService.delete(id);
   }
 }
